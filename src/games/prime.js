05c8619b8b2296cdaf6abe62112f3b52dcb4d02c
2vlad <vlad.kiaune@gmail.com>
2019-04-16 14:13:18 +0300

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,6 +4,9 @@ import random from '../utils';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
+  if (num < 3) {
+    return false;
+  }
   for (let i = 2; i < num / 2; i += 1) {
     if (num % i === 0) {
       return false;
